82aec5c9dc55fe6df2a814054b04ec3807156b62
PowerSnail <hj@powersnail.com>
2023-01-12 02:55:37 -0800

    add delim char to output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use clap::Parser;
 use nom::bytes::complete::{tag, take_till1, take_while1};
 use nom::character::complete::digit1;
@@ -25,6 +27,18 @@ struct Options {
     fields: String,
 }
 
+fn print_row<T>(cells: impl Iterator<Item = T>)
+where
+    T: Display,
+{
+    let mut delim = "";
+    for i in cells {
+        print!("{}{}", delim, &i);
+        delim = "\t";
+    }
+    println!("");
+}
+
 fn main() {
     let opts = Options::parse();
     let (_, field_ids) = parse_fields(&opts.fields).expect("Failed to parse fields.");
@@ -32,9 +46,6 @@ fn main() {
     for line in std::io::stdin().lines() {
         let line = line.expect("Failed to read from standard input");
         let (_, fields) = parse_line(&line).expect(&format!("Failed to parse input: {}", line));
-        for i in &field_ids {
-            print!("{}", fields[*i]);
-        }
-        println!("");
+        print_row(field_ids.iter().map(|&i| fields[i]));
     }
 }
