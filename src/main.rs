d1e54c21d02403b04cc4720289c4a4d8f8bbd76d
PowerSnail <hj@powersnail.com>
2023-01-12 03:40:14 -0800

    improve error messages

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 use clap::Parser;
 use nom::bytes::complete::{tag, take_till1, take_while1};
 use nom::character::complete::digit1;
-use nom::combinator::map_res;
+use nom::combinator::{all_consuming, map_res};
 use nom::multi::{separated_list0, separated_list1};
 use nom::IResult;
 
@@ -19,11 +19,14 @@ fn parse_usize(i: &str) -> IResult<&str, usize> {
 }
 
 fn parse_fields(i: &str) -> IResult<&str, Vec<usize>> {
-    separated_list1(tag(","), parse_usize)(i)
+    all_consuming(separated_list1(tag(","), parse_usize))(i)
 }
 
 #[derive(clap::Parser)]
+#[command(author, version, about)]
 struct Options {
+    /// In the format of comma delimited positive integers. At least one field must be specified.
+    /// The field number starts from 0.
     fields: String,
 }
 
@@ -39,13 +42,36 @@ where
     println!("");
 }
 
+trait ErrorAndExit<R, E> {
+    fn unwrap_or_exit(self, msg: &str) -> R
+    where
+        E: Display;
+}
+
+impl<R, E> ErrorAndExit<R, E> for Result<R, E> {
+    fn unwrap_or_exit(self, msg: &str) -> R
+    where
+        E: Display,
+    {
+        match self {
+            Ok(v) => v,
+            Err(e) => {
+                eprintln!("{}: {}", msg, &e);
+                std::process::exit(1);
+            }
+        }
+    }
+}
+
 fn main() {
     let opts = Options::parse();
-    let field_ids = parse_fields(&opts.fields).expect("Failed to parse fields.").1;
+    let field_ids = parse_fields(&opts.fields)
+        .unwrap_or_exit("Faile to parse fields")
+        .1;
 
     for line in std::io::stdin().lines() {
-        let line = line.expect("Failed to read from standard input");
-        let fields = parse_line(&line).expect(&format!("Failed to parse input: {}", line)).1;
+        let line = line.unwrap_or_exit("Failed to fetch line from stdin");
+        let fields = parse_line(&line).unwrap_or_exit("Failed to parse input").1;
         print_row(field_ids.iter().map(|&i| fields[i]));
     }
 }
